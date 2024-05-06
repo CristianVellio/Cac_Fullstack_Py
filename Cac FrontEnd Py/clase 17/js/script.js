71f0b9020cf4a2da3b32325591cc7fce38a5ed31
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-05-06 00:33:08 -0300

    added class

---
+++ Cac FrontEnd Py/clase 17/js/script.js
@@ -0,0 +1,9 @@
+//Crear la clase libros
+
+class Libros {
+  constructor(tit, aut, lan, sto) {
+    (titulo = tit), (autor = aut), (lanzamiento = lan), (stock = sto);
+  }
+}
+
+const libro1 = new Libros("Cuentos", "R.Fontanarrosa", 1970, 35);
--- Cac FrontEnd Py/clase 17/script.js
+++
@@ -1,9 +0,0 @@
-//Crear la clase libros
-
-class Libros {
-  constructor(tit, aut, lan, sto) {
-    (titulo = tit), (autor = aut), (lanzamiento = lan), (stock = sto);
-  }
-}
-
-const libro1 = new Libros("Cuentos", "R.Fontanarrosa", 1970, 35);
