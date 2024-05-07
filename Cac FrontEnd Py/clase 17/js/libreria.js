20561d2570c9800901655a984558afc5c10b5f40
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-05-07 02:32:06 -0300

    added functions

--- Cac FrontEnd Py/clase 17/js/libreria.js
+++ Cac FrontEnd Py/clase 17/js/libreria.js
@@ -9,4 +9,21 @@ class Libros {
   }
 }
 
-const libro1 = new Libros("Cuentos", "R.Fontanarrosa", 1970, 35);
+const libros = [];
+
+function crearLibro(tit, aut, lan, sto) {
+  const nuevoLibro = new Libros(tit, aut, lan, sto);
+  libros.push(nuevoLibro);
+}
+
+crearLibro("Cuentos", "R.Fontanarrosa", 1970, 35);
+crearLibro("Otros cuentos", "Jorge Luis Borges", 1940, 23);
+crearLibro("Mas cuentos", "E. Sachieri", 2006, 67);
+
+// DOM - Document Object Model
+
+let contendor = document.querySelector("#libros-oferta");
+
+for (const i of libros) {
+  contendor.innerHTML += "<div>" + i.autor + "</div>";
+}
